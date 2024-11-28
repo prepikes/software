f046b85e0c95a81aa1c6486615563e5f858fd1ca
prepikes <494526342@qq.com>
2024-11-28 17:12:21 +0800

    Start

---
+++ HotelSystem/UsingAC/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UsingacConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "UsingAC"
---
+++ HotelSystem/UsingAC/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("请求路径:{}" .format(request.path))
+# Create your views here.
